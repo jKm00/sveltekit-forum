31b6e05e588870a28ec7a3727b52cf1179f53a65
jKm00 <joakimedvardsen2000@gmail.com>
2023-05-10 00:20:47 +0200

    change adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
-import { dev } from '$app/environment';
-import adapter from '@sveltejs/adapter-auto';
-import netlifyAdapter from '@sveltejs/adapter-netlify'
+import adapter from '@sveltejs/adapter-netlify'
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: dev ? adapter() : netlifyAdapter()
+		adapter: adapter()
 	}
 };
 
